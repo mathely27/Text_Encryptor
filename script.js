5f750328af6aecc11f89c3bb35b7c98510a6e00d
mathely mittal <mathely@mathely.mathely>
2026-05-17 11:36:02 +0530

    Updated UI and styling

--- script.js
+++ script.js
@@ -9,7 +9,46 @@ const encryptResult = document.getElementById("encrypt-result")
 encryptBtn.addEventListener("click", function(){
     const text = encryptInput.value
 
-    encryptResult.innerText = text
+    encryptResult.innerText = btoa(text)
 
 })
 
+
+const decryptInput=document.getElementById("decrypt-input")
+
+const decryptBtn=document.getElementById("decrypt-btn")
+
+const decryptResult=document.getElementById("decrypt-result");
+
+decryptBtn.addEventListener("click",function(){
+    const text= decryptInput.value
+    try{
+    decryptResult.innerText=atob(text)
+    }
+    catch{
+        decryptResult.innerText = "Invalid encrypted text"
+    }
+})
+
+const copyBtn=document.getElementById("copy-btn")
+
+copyBtn.addEventListener("click",function(){
+    const text = encryptResult.innerText
+    navigator.clipboard.writeText(text)
+    alert("Copied ti clioboard!!")
+    
+    
+
+})
+
+// encryptInput.addEventListener("keydown", function(event){
+
+//     event.preventDefault()
+//     if(event.key=="Enter"){
+//         const text = encryptInput.value
+
+//         encryptResult.innerText = btoa(text)
+
+//     }
+
+// })
